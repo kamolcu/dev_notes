d6607148a0bb00ec7fee1e6559c68b5b95c3743b
kamol <kamolcu@gmail.com>
2016-01-28 17:56:15 +0700

    Add ACK logic

--- python/stomp_activemq.py
+++ python/stomp_activemq.py
@@ -2,19 +2,26 @@
 import stomp
 import time
 
+
 class MessageListener(stomp.ConnectionListener):
+
+    def __init__(self, con=None):
+        self.con = con
 
     def on_error(self, headers, message):
         print('received an error "%s"' % message)
 
     def on_message(self, headers, message):
-        print('received a message "%s"' % message)
+        print('received a message "%s" and headers "%s"' % (message, headers))
+        if(self.con is not None):
+            self.con.ack(headers["message-id"], headers["subscription"])
+
 
 con = stomp.Connection([('ba-server.com', 61613)])
-con.set_listener('', MessageListener())
+con.set_listener('', MessageListener(con=con))
 con.start()
 con.connect('admin', 'admin', wait=True)
-con.subscribe(destination='/queue/test', id=1, ack='auto')
+con.subscribe(destination='/queue/test', id=1, ack='client-individual')
 
 while True:
     time.sleep(1)
